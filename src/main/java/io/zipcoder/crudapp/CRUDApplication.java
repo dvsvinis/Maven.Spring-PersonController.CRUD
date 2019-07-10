4ff53806e86047aa03e5ac71d431975021ae3b61
donnasvinis <dvsvinis@mac.com>
2019-07-10 18:54:46 -0400

    Initial commit

--- src/main/java/io/zipcoder/crudapp/CRUDApplication.java
+++ src/main/java/io/zipcoder/crudapp/CRUDApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+//trying to initial commit
 @SpringBootApplication
 public class CRUDApplication {
 
