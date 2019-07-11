8ef6b4ecff428f745a079e08c69030e9bdb3c591
donnasvinis <dvsvinis@mac.com>
2019-07-11 07:26:14 -0400

    Defining classes

--- src/main/java/io/zipcoder/crudapp/CRUDApplication.java
+++ src/main/java/io/zipcoder/crudapp/CRUDApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-//trying to initial commit
+
 @SpringBootApplication
 public class CRUDApplication {
 
